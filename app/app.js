96de9d9b4ddad8119ca7584d09571b2f8437383c
Pedro J. Martínez <pedrojmrt@gmail.com>
2016-10-27 14:36:28 +0200

    added isValidCoord, addEvents, new settersand getters

--- app/app.js
+++ app/app.js
@@ -1,29 +1,57 @@
 class Game {
 	/*
-	Handles the game
-	Logical part.
+		Handles the game
+		Logical part.
 
-	ATTRIB:
-		- coords. After coords is built, stores
-		coords data.
+		ATTRIB:
+			- coords. After coords is built, stores
+			coords data.
+			- currentCoord. Stores the zero cell
+			coord which user plays.
 	*/
 	constructor() {
-		this.coords = []
+		this.coords = [];
+		this.currentCoord = [];
+	}
+
+	set changeCurrentCoord(coord){
+		/*
+			Changes the 0 cell currentCoord.
+			If you want to do any checks do 'em over here.
+		*/
+		this.currentCoord = [];
+		this.currentCoord.push(...coord);
+	}
+
+	get playerCell(){
+		/*
+			Returns the player current
+			cell which is playing at this
+			time.
+
+			@Return: array ([row, col])
+		*/
+		return this.currentCoord;
 	}
 
 	// TODO WITH SET
 	setCoords(startingOrder) {
 		/*
-		Gets the startingOrder (array),
-		stores the coords.
+			Gets the startingOrder (array),
+			stores the coords.
 		*/
 		let currentOrder = startingOrder.slice();
 		let dimension = Math.sqrt(startingOrder.length);
 
 		for (let i=0; i < dimension; i++){
-			let row = []
+			let row = [];
 			for (let j=0; j < dimension; j++){
-				row.push(currentOrder.shift());
+				let rowValue = currentOrder.shift()
+				row.push(rowValue);
+
+				if (rowValue == 0) {
+					this.changeCurrentCoord = [i, j];
+				}
 			}
 			this.coords.push(row);
 		}
@@ -31,7 +59,8 @@ class Game {
 
 	getCoords(){
 		/*
-		Return the Coords if
+			Return the Coords if this.coords
+			is not empty.
 		*/
 		if(!Utils.isEmpty(this.coords)) {
 			return this.coords;
@@ -39,7 +68,6 @@ class Game {
 			return false;
 		}
 	}
-
 
 }
 
@@ -50,6 +78,32 @@ class Tracker {
 
 	}
 
-	isValidMove() {
+	isValidCoord(coord, dimension) {
+		/*
+			Checks if a coord is
+			valid.
+
+			MUST-BE Conditions:
+				- coord <= dimension
+				- coord > 0
+
+			Notes:
+				- Direction stands for row or col.
+
+			@Return: boolean
+		*/
+		let valid = true;
+
+		for (let direction of coord) {
+			if (direction > dimension-1 || direction < 0) {
+				valid = false;
+			}
+		}
+		return valid;
 	}
+
+	addEvents(){
+		document.addEventListener("keydown", onKeyDown);
+	}
+
 }
